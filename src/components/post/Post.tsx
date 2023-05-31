363305afe71338d599915d0bb8488a6eebcf3717
builtbysuraj <surajgupta3553@gmail.com>
2023-05-31 23:59:51 +0530

    style: style in progress of input field

--- src/components/post/Post.tsx
+++ src/components/post/Post.tsx
@@ -1,5 +1,39 @@
+import {
+  AccountCircle,
+  EmojiEmotions,
+  PermMedia,
+  PersonAddAlt1,
+} from "@mui/icons-material"
+
 import "./Post.scss"
 
 export default function Post() {
-  return <div className="post-container">Post</div>
+  return (
+    <main className="post-container">
+      <div className="input-card">
+        <section className="input-section">
+          <AccountCircle className="input-avatar-icon" />
+          <input type="text" placeholder="What's on your mind, Josephine?" />
+        </section>
+        <span className="hr">
+          <hr />
+        </span>
+        <section className="options">
+          <section className="options-item">
+            <PermMedia className="media-icon icon" />
+            <section className="option-name">Photo/Video</section>
+          </section>
+          <section className="options-item">
+            <PersonAddAlt1 className="tag-person-icon icon" />
+            <section className="option-name">Tag Friends</section>
+          </section>
+          <section className="options-item">
+            <EmojiEmotions className="feeling-icon icon" />
+            <section className="option-name">Feeling/Activity</section>
+          </section>
+        </section>
+      </div>
+      <div className="posts-wrapper"></div>
+    </main>
+  )
 }
